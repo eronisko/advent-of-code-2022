27cc2938873395a13f6aa25266419f77f13f0252
Ernest Walzel <ewalzel@gmail.com>
2022-12-08 18:45:00 +0100

    Day 5

---
+++ day-05/part-1.js
@@ -0,0 +1,56 @@
+import { createReadStream } from "fs";
+import path from "path";
+import { createInterface } from "readline";
+import { strict as assert } from "node:assert";
+import { fileURLToPath } from "url";
+
+async function solution(filename, stacks) {
+  const lines = createInterface({
+    input: createReadStream(filename),
+  });
+
+  let instructionsReached = false;
+
+  for await (const line of lines) {
+    if (line === "") {
+      instructionsReached = true;
+      continue;
+    }
+    if (!instructionsReached) continue;
+
+    const [_, howMany, __, from, ___, to] = line
+      .split(" ")
+      .map((part) => parseInt(part));
+
+    for (let moveCounter = 1; moveCounter <= howMany; moveCounter++) {
+      const container = stacks[from - 1].pop();
+      stacks[to - 1].push(container);
+    }
+  }
+
+  return stacks.map((stack) => stack[stack.length - 1]).join("");
+}
+
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+
+assert.equal(
+  await solution(path.join(currentDir, "example.txt"), [
+    Array.from("ZN"),
+    Array.from("MCD"),
+    Array.from("P"),
+  ]),
+  "CMZ"
+);
+console.log(
+  await solution(path.join(currentDir, "input.txt"), [
+    Array.from("RPCDBG"),
+    Array.from("HVG"),
+    Array.from("NSQDJPM"),
+    Array.from("PSLGDCNM"),
+    Array.from("JBNCPFLS"),
+    Array.from("QBDZVGTS"),
+    Array.from("BZMHFTQ"),
+    Array.from("CMDBF"),
+    Array.from("FCQG"),
+  ])
+);
